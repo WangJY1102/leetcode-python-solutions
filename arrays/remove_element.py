ad2657946c825a3382d235a40a3026c006509ff8
WangJY1102 <1239857985@qq.com>
2025-11-20 16:16:35 +0800

    feat: improvement in arrays/remove_element.py

--- arrays/remove_element.py
+++ arrays/remove_element.py
@@ -2,7 +2,10 @@ from typing import List
 
 class Solution:
     def removeElenment(self, nums: List[int], val: int) -> int:
-        """用栈思考，实现元素移除，把nums本身看作一个栈"""
+        """用栈思考，实现元素移除，把nums本身看作一个栈
+        其实本质可以看作是双指针：右指针 right(for循环)指向当前将要处理的元素，左指针 left（stack_size） 指向下一个将要赋值的位置。
+        时间复杂度：O(n)，其中 n 是 nums 的长度
+        空间复杂度：O(1)"""
         stack_size = 0
         for num in nums:
             if num != val:
